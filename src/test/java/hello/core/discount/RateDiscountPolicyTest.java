e42a172adfa90a281ac4c9d72f0dfe33fe92c261
sunghyun <a91078933@gmail.com>
2023-06-21 23:03:59 +0900

    메뉴 테스트 케이스 작성

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -6,10 +6,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
-    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+    RateDiscountPolicy DiscountPolicy = new RateDiscountPolicy();
 
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
@@ -19,12 +20,21 @@ class RateDiscountPolicyTest {
         Member member = new Member(1L,"memberA",Grade.VIP);
 
         //when
-        int dicount = rateDiscountPolicy.discount(member,10000);
+        int dicount = DiscountPolicy.discount(member,10000);
 
         //then
         Assertions.assertThat(dicount).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("VIP가 아니면")
+    void vio_x(){
+        Member member = new Member(2L,"memberB",Grade.BASIC);
+
+        int discount = DiscountPolicy.discount(member,10000);
+
+        assertThat(discount).isEqualTo(0);
+    }
 
 
 }
