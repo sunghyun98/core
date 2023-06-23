fb9f3d8aed4e8c30d207c1a9d37e70c7e545abc1
sunghyun <a91078933@gmail.com>
2023-06-23 23:55:38 +0900

    인터페이스 구현클래스 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,21 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy ) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberID, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberID);
@@ -16,4 +23,5 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberID, itemName, itemPrice, discountPrice);
     }
+
 }
