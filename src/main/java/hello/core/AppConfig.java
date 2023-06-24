44bae357be5f93504531e92505ba52038ece54e2
sunghyun <a91078933@gmail.com>
2023-06-24 20:25:35 +0900

    의존관계 주입 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -16,17 +17,18 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
-    }
-
-
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+       // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
+
+
 }
