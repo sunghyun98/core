7b9fdec6753cfdd2da15cb8148e74ed6073ef604
sunghyun <a91078933@gmail.com>
2023-06-24 23:26:28 +0900

    의존관계 주입 수정 -> 스프링전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,8 +10,13 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//애플리케이션의 설정정보
+@Configuration
 public class AppConfig {
+/*
     //생성한 객체 인스턴스의 참조를 생성자를 통해서 주입한다.
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
@@ -29,6 +34,28 @@ public class AppConfig {
        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+*/
 
+    //스프링 컨테이너 등록
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
 
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
         //MemberServiceImpl 클래스의 객체가 생성됩니다. 이 객체는 MemberServiceImpl 클래스의 생성자를 호출하여 초기화됩니다.
         //생성된 MemberServiceImpl 객체의 참조가 MemberService 타입의 변수인 memberService에 할당됩니다.
         //이제 memberService 변수를 사용하여 MemberService 인터페이스에 선언된 메소드를 호출할 수 있습니다.
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
