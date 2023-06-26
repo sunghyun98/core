e594ddc1fcaaf800a5768d0ef88b8c0c354ea99a
sunghyun <a91078933@gmail.com>
2023-06-26 21:38:57 +0900

    bean 조회 자세히

---
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,10 @@
+package hello.core.beanfind;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+
+
+
+}
