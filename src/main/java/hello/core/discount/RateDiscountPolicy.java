695d116ca55455a9bbc2631d09762601835a387a
sunghyun <a91078933@gmail.com>
2023-07-02 23:28:07 +0900

    @Component

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent = 10;
-
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
