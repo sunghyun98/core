79f750af0b5180a9848bfa662ba146930e5d2bbe
sunghyun <a91078933@gmail.com>
2023-07-04 17:07:01 +0900

    ComponentFilter -includeFilters -excludeFilters

---
+++ src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,10 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+//TYPE : 클래스 레벨에 붙는다
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
