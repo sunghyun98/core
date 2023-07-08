5d24c9c8d7c6e289b006fc33618bd91083f19c22
sunghyun <a91078933@gmail.com>
2023-07-08 23:31:43 +0900

    의존성 선택

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -19,6 +19,7 @@ class OrderServiceImplTest {
         OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
         Order order = orderService.createOrder(1L, "itemA", 10000);
 
+
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
