80a3706a245944678193975997c7cd0ddc3e720b
sunghyun <a91078933@gmail.com>
2023-07-08 23:11:47 +0900

    의존성 선택

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -30,7 +30,6 @@ public class OrderServiceImpl implements OrderService{
     public Order createOrder(Long memberID, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberID);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-
         return new Order(memberID, itemName, itemPrice, discountPrice);
     }
 
