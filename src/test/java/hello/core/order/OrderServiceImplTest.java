b48aa8270d7111e898599f3c8502181861801dec
sunghyun <a91078933@gmail.com>
2023-07-09 21:30:05 +0900

    의존성 선택

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
-
     @Test
     void creatOrder(){
         MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
