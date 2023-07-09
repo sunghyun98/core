0615d8c4f47a6c18b1380bb99933937f6c9ed1f4
sunghyun <a91078933@gmail.com>
2023-07-09 22:16:09 +0900

    의존성 선택

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         basePackages = "hello.core.member",
         basePackageClasses = AutoAppConfig.class,
-        //컴포넌트 스캔에서 뺄것을 지정
+        //컴포넌트 스캔에서 뺄것을 지정dd
         //Configuration.class을 빼는 이유는 수동으로 등록한 AppConfig를 제거해야지 예제 클래스가 충돌이 안 일어남
         //기존 AppConfig와 다르게 @Bean을 등록한것이 없다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
