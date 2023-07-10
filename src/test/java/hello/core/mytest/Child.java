b5d21fffb252cf73ec6bc12b45b4a0db24a04158
sunghyun <a91078933@gmail.com>
2023-07-10 23:39:53 +0900

    정처기 실습 예쩨

---
+++ src/test/java/hello/core/mytest/Child.java
@@ -0,0 +1,18 @@
+package hello.core.mytest;
+
+class Child extends Parent {
+    public Child(int age, String name){
+        System.out.println("자식 생성자");
+    }
+
+    public Child(){
+        this.print();
+        this.age = 1;
+        this.name = "SD";
+        int age = 1;
+        String name = "dsd";
+        System.out.println("fdfdfd");
+    }
+
+}
+
--- src/test/java/hello/core/mytest/dd.java
+++ src/test/java/hello/core/mytest/dd.java
@@ -2,9 +2,9 @@ package hello.core.mytest;
 
 public class dd {
     public static void main(String[] args) {
-        ddf1 p1 = new ddf1();
-        p1.f1();
-        p1.f2();
-        p1.f3();
+
+        Child b = new Child();
+        b.print();
+
     }
 }
