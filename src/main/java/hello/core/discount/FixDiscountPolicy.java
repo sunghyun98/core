b90488b5e629022865de7b1994c70e2db076d2d6
sunghyun <a91078933@gmail.com>
2023-07-28 23:43:43 +0900

    annotation 생성

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FixDiscountPolicy implements DiscountPolicy{
+
     private  int discountFixAmount = 1000; // 1000원 할인
     @Override
     public int discount(Member member, int price) {
