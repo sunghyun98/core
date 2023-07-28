164180a0e5b788418163642d99c577d759493fc2
sunghyun <a91078933@gmail.com>
2023-07-28 23:37:44 +0900

    annotation 생성

--- src/main/java/hello/core/HelloLombok.java
+++ src/main/java/hello/core/HelloLombok.java
@@ -2,9 +2,11 @@ package hello.core;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class HelloLombok {
     private String name;
     private int age;
@@ -12,8 +14,11 @@ public class HelloLombok {
     public static void main(String[] args) {
         HelloLombok helloLombok = new HelloLombok();
         helloLombok.setName("asdf");
+        helloLombok.setAge(12);
         
         String name = helloLombok.getName();
         System.out.println("name = " + name);
+
+        System.out.println(helloLombok);
     }
 }
