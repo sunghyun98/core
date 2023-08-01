5c0291e7c136f6d31f8738d145593bac283b8545
sunghyun <a91078933@gmail.com>
2023-08-01 23:42:39 +0900

    annotation 생성

--- src/test/java/hello/core/Autowired/AllBeanTest.java
+++ src/test/java/hello/core/Autowired/AllBeanTest.java
@@ -45,6 +45,7 @@ public class AllBeanTest {
             System.out.println("policies = " + policies);
         }
 
+
         public int discount(Member member, int price, String discountCode) {
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
             return discountPolicy.discount(member,price);
