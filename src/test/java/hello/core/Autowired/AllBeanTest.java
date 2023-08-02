077c9e449880327f0452e2d1d8c550b750299d14
sunghyun <a91078933@gmail.com>
2023-08-02 23:26:55 +0900

    annotation 생성

--- src/test/java/hello/core/Autowired/AllBeanTest.java
+++ src/test/java/hello/core/Autowired/AllBeanTest.java
@@ -37,6 +37,7 @@ public class AllBeanTest {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
+
         @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
