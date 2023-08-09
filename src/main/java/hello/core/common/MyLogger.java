5e1ce1f8e3ba513d1092ba7ad0c63849568d6891
sunghyun <a91078933@gmail.com>
2023-08-09 23:14:43 +0900

    /

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 public class MyLogger {
     private String uuid;
     private String requestURL;
-
     public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
