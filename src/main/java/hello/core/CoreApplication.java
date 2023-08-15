9b5b400e9f1800ce74f59ff39e0f093a32d36feb
sunghyun <a91078933@gmail.com>
2023-08-15 20:41:33 +0900

    MyLogger

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
-
 	}
 }
