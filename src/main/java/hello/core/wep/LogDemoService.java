14f5295a411d0636877350ccaf7abaf1ccaf8b23
sunghyun <a91078933@gmail.com>
2023-08-27 23:53:09 +0900

    LogDemoService

--- src/main/java/hello/core/wep/LogDemoService.java
+++ src/main/java/hello/core/wep/LogDemoService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-
     private final MyLogger myLogger;
     public void logic(String id) {
         myLogger.log("service id = " + id);
