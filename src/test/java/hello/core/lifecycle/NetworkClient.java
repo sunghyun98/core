ecd8905dac2e8e1b8513c6dc81b5589ed94f68ff
sunghyun <a91078933@gmail.com>
2023-08-31 23:42:42 +0900

    NetworkClient

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -10,7 +10,6 @@ public class NetworkClient {
     private String url;
     public  NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-
     }
 
     public void setUrl(String url){
