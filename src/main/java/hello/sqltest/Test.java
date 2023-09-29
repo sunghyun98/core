40f3a88a7cf98bea8d75b6545cf56d0beda3b7fa
sunghyun <a91078933@gmail.com>
2023-09-29 23:20:10 +0900

    .

---
+++ src/main/java/hello/sqltest/Test.java
@@ -0,0 +1,15 @@
+package hello.sqltest;
+
+public class Test {
+    void f(){
+        System.out.println("1");
+    }
+    void f(int i){
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test();
+        a.f();
+    }
+}
